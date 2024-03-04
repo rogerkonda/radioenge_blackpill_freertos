89a2f93ea7c6d6d19c7ec4ed86f2f83cc41b82d7
rogerkonda <rogerkonda@gmail.com>
2024-03-04 15:28:39 -0300

    comentário

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+/* ------> COMENTÁRIO DE TESTE ROGER <------- */
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
